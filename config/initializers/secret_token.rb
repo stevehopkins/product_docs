f2baff9c0afd768204d7cf5411d6dd5ba7975c66
Stevehopkins <steve@thesquigglyline.com>
2011-06-14 16:11:20 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProductDocs::Application.config.secret_token = 'dd72807b9d08244bd2d6ddff45512a8944b90a97d34a4aac1e69b6de316c158700b34694db304b173749b7cdb8348fe9540ded666e80bd1947581681506215f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProductDocs::Application.config.session_store :cookie_store, :key => '_product_docs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProductDocs::Application.config.session_store :active_record_store
